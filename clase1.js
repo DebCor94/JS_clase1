3732a70c10a3a0d56c7fdab59c9b09652423a2e2
DebCor94 <debora.coronel94@gmail.com>
2021-09-29 22:23:24 -0300

    clase1_operaciones

---
+++ clase1.js
@@ -0,0 +1,16 @@
+// Clase 1 - Desafío obligatorio
+
+// Variables y valores
+const BLANCO = " "
+var prefijo = 549
+
+// Uso de prompt y alert
+var nombreCliente = prompt ("Ingrese su nombre y apellido");
+console.log(nombreCliente)
+
+var numeroCel= parseInt(prompt("Ingrese su número de teléfono"))
+console.log(numeroCel)
+
+alert("Bienvenido otra vez," + BLANCO + nombreCliente)
+alert("Se registró con el número:" + BLANCO + prefijo + numeroCel)
+
